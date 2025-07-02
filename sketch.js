1ccacae3696403513ec39e155ef5833c5852a0c6
Fentonnh <fentonnh@gmail.com>
2025-07-01 22:11:53 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let inkShader;
+let textGraphic;
+
+function preload() {
+  inkShader = loadShader("shader.vert", "shader.frag");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+
+  textGraphic = createGraphics(windowWidth, windowHeight);
+  textGraphic.pixelDensity(1);
+  textGraphic.background(255);
+  textGraphic.fill(0);
+  textGraphic.textAlign(CENTER, CENTER);
+  textGraphic.textSize(36);
+  textGraphic.textFont("Georgia");
+  textGraphic.text(
+    "The unknown is not your enemy,\nit is your birthplace.",
+    textGraphic.width / 2,
+    textGraphic.height / 2
+  );
+}
+
+function draw() {
+  shader(inkShader);
+  inkShader.setUniform("u_time", millis() / 1000.0);
+  inkShader.setUniform("u_resolution", [width, height]);
+  inkShader.setUniform("u_text", textGraphic);
+
+  rect(-width / 2, -height / 2, width, height);
+}
