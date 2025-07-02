c2a302aae5e2af8664c4e09895f4ef1164f0dacb
Fentonnh <fentonnh@gmail.com>
2025-07-01 22:18:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let inkShader;
 let textGraphic;
+let quoteInput;
 
 function preload() {
   inkShader = loadShader("shader.vert", "shader.frag");
@@ -9,15 +10,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 
+  // Reference the HTML text area
+  quoteInput = document.getElementById("quoteInput");
+  quoteInput.value = "The unknown is not your enemy,\nit is your birthplace.";
+
+  // Create off-screen graphics for the quote
   textGraphic = createGraphics(windowWidth, windowHeight);
   textGraphic.pixelDensity(1);
+  updateTextGraphic();
+
+  // Update whenever the input changes
+  quoteInput.addEventListener("input", updateTextGraphic);
+}
+
+function updateTextGraphic() {
+  textGraphic.clear();
   textGraphic.background(255);
   textGraphic.fill(0);
   textGraphic.textAlign(CENTER, CENTER);
   textGraphic.textSize(36);
   textGraphic.textFont("Georgia");
   textGraphic.text(
-    "The unknown is not your enemy,\nit is your birthplace.",
+    quoteInput.value,
     textGraphic.width / 2,
     textGraphic.height / 2
   );
@@ -28,6 +42,5 @@ function draw() {
   inkShader.setUniform("u_time", millis() / 1000.0);
   inkShader.setUniform("u_resolution", [width, height]);
   inkShader.setUniform("u_text", textGraphic);
-
   rect(-width / 2, -height / 2, width, height);
 }
